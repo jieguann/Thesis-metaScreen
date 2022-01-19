e53930963239cfe7dd4fec8fb43b605ca915c7fa
jieguann <60665347+jieguann@users.noreply.github.com>
2022-01-19 03:04:30 -0500

    Add time count to javascript when user inside the box

---
+++ 2_webcam/sketch.js
@@ -0,0 +1,132 @@
+// ml5.js: Object Detection with COCO-SSD (Webcam)
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/learning/ml5/1.3-object-detection.html
+// https://youtu.be/QEzRxnuaZCk
+
+// p5.js Web Editor - Image: https://editor.p5js.org/codingtrain/sketches/ZNQQx2n5o
+// p5.js Web Editor - Webcam: https://editor.p5js.org/codingtrain/sketches/VIYRpcME3
+// p5.js Web Editor - Webcam Persistence: https://editor.p5js.org/codingtrain/sketches/Vt9xeTxWJ
+
+// let img;
+let video;
+let detector;
+let detections = [];
+let object;
+
+const widthC = 680;
+const heightC = 480;
+
+//set time
+let totalSecond = 0;
+
+
+function preload() {
+  // img = loadImage('dog_cat.jpg');
+  detector = ml5.objectDetector('cocossd');
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function setup() {
+  //put canvas in div html
+  var canvas = createCanvas(widthC, heightC);
+  canvas.parent('canvasForHTML');
+
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  detector.detect(video, gotDetections);
+}
+
+function draw() {
+
+  drawDetection();
+  stroke("red");
+  noFill();
+  rect(widthC/4,20, widthC/2, heightC-20);
+
+}
+
+function drawDetection(){
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i++) {
+  // if(detections.length >0){
+    object = detections[i];
+
+
+    if(object.label=="person"){
+
+
+      stroke(0, 255, 0);
+      strokeWeight(4);
+      noFill();
+      rect(object.x, object.y, object.width, object.height);
+      noStroke();
+      fill(255);
+      textSize(24);
+      text(object.label, object.x + 10, object.y + 24);
+
+      if(object.x < widthC/4+widthC/4 && object.x > widthC/4-widthC/4){
+        // console.log(performance.now());
+        totalSecond++;
+        let minutes = Math.floor(totalSecond/3600);
+        let seconds = Math.floor((totalSecond - hour *3600)/60);
+        let milliseconds = totalSecond - (hour*3600 + minute*60);
+        console.log(minutes + " " + seconds +" " + milliseconds);
+      }
+
+      else{
+
+      }
+
+    }
+
+  }
+
+
+  //empty personArray
+  // calculate how many people are in the frame
+  // console.log(personArray.length);
+  //turn number of people into a string
+  // let numPeople = personArray.length.toString();
+  // client.publish('acelab/humanpresent', "hello", { qos: 0, retain: false })
+
+
+
+
+}
+
+
+
+//Code for MQTT
+
+// const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+//
+// const host = 'wss://mqtt.eclipseprojects.io:443/mqtt'
+//
+// console.log('Connecting mqtt client')
+// const client = mqtt.connect(host)
+//
+// client.on('error', (err) => {
+// console.log('Connection error: ', err)
+// client.end()
+// })
+//
+// client.on('reconnect', () => {
+// console.log('Reconnecting...')
+// })
+//
+// client.on('message', (topic, message, packet) => {
+//   console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic)
+// })
+
+
+
+
